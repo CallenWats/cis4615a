16f20e78aebc18b063c1c17ed763aac84b92debb
CallenWats <57461196+CallenWats@users.noreply.github.com>
2019-11-07 17:24:52 -0500

    Update R04_STR03_J.java

--- R04_STR03_J.java
+++ R04_STR03_J.java
@@ -4,14 +4,32 @@ public class R04_STR03_J {
 
     public static void main(String[] args){
     
+        /******************************************************************************
+     *  Compilation:  javac R03_NUM03_J.java
+     *  Execution:    java R03_NUM03_J
+     * 
+     *  Converts BigInteger x to a string->byte[]->string->BigInteger.
+     *  Displays the before and after to prove that both iterations of x are the same
+     * 
+     ******************************************************************************/
+    public static void main(String[] args){
+    
         BigInteger x = new BigInteger("530500452766");
         
         System.out.println(x.toString());
         
-        byte[] byteArray = x.toByteArray();
-        String s = new String(byteArray);
-        byteArray = s.getBytes();
-        x = new BigInteger(byteArray);
+        /*     
+        * Rule 04. Characters and Strings (STR)    
+        * Corrected code per:     
+        * https://wiki.sei.cmu.edu/confluence/display/java/STR03-J.+Do+not+encode+noncharacter+data+as+a+string    
+        *     
+        * Rule 04-STR03-J
+        */
+        
+        String s = x.toString();  // Valid character data
+        byte[] byteArray = s.getBytes();
+        String ns = new String(byteArray); 
+        x = new BigInteger(ns);
         
         System.out.println(x.toString());
         
