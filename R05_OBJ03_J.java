5119f5355a73fa1c7cf6afbafafb4740fa880c5f
CallenWats <57461196+CallenWats@users.noreply.github.com>
2019-11-07 19:14:40 -0500

    Update R05_OBJ03_J.java

--- R05_OBJ03_J.java
+++ R05_OBJ03_J.java
@@ -1,12 +1,36 @@
 public class R05_OBJ03_J {
   
-  private static void addToList(List list, Object obj) {
-    list.add(obj); // Unchecked warning
+  /******************************************************************************
+     *
+     * Compilation:  javac R05_OBJ03_J.java
+     *  Execution:    java R05_OBJ03_J
+     * 
+     *  Calls addToList() and adds a given string to the list. Loops through a for
+     *  loop 10 times adding numbers and a single instance with a word.
+     * 
+     ******************************************************************************/
+    
+  
+  /*     
+    * Rule 05. Object Orientation (OBJ)    
+    * Corrected code per:     
+    * https://wiki.sei.cmu.edu/confluence/display/java/OBJ03-J.+Prevent+heap+pollution    
+    *     
+    * Rule 05-OBJ03-J
+    */
+    private static void addToList(List<String> list, String str) {
+    list.add(str);     // No warning generated
   }
  
   public static void main(String[] args) {
     List<String> list = new ArrayList<String> ();
-    addToList(list, 42);
-    System.out.println(list.get(0));  // Throws ClassCastException
+    for(int x = 0; x < 10; x++){
+        addToList(list, Integer.toString(x));
+    }
+    for(int x = 0; x < 10; x++){
+    System.out.println(list.get(x));
+  
+    }
   }
+    
 }
